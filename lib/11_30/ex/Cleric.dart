a780ea37a4c2d6665cfea278a5cb6b423715c178
Hyojun Cho <lionjerher84@gmail.com>
2023-11-30 18:25:21 +0900

    1-4 pray() 구현

---
+++ lib/11_30/ex/Cleric.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+class Cleric {
+  String name = "아벨";
+  final int maxHp = 50;
+  final int maxMp = 10;
+  int hp;
+  int mp;
+
+  Cleric(this.name, this.hp, this.mp);
+
+  /// 셀프에이드 : mp를 소모해서, hp 회복한다.
+  void selfAid() {
+    mp = -10; // mp10 소모
+    hp = maxHp; // 최대 hp까지 회복
+  }
+
+  /// 기도하기 : 자신의 mp 회복한다.
+  /// 기도한 시간(초) : 랜덤 0 ~ 2
+  /// param : int prayTime / return int result;
+  int pray(int prayTime) {
+    print('maxMp : $maxMp');
+    print('mp : $mp');
+
+    int result = 0;
+    int point = Random().nextInt(3);
+
+    if (mp < maxMp) result += prayTime + point;
+
+    // maxMp 이하 검증
+    if ((mp + result) > maxMp) result = maxMp - mp;
+
+    print('회복시간: $prayTime / 회복량: $point / 최종 회복량: $result');
+
+    return result;
+  }
+
+}
---
+++ lib/11_30/ex/main.dart
@@ -0,0 +1,15 @@
+
+
+import 'Cleric.dart';
+
+void main() {
+
+  // 테스트 코드
+  // mp: 8 가정
+  Cleric cl = Cleric("용자", 50, 5);
+
+  // 회복시간 5초
+  cl.pray(3);
+
+
+}
