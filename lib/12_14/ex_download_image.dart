be9ad004133ec67efb6a218f6ef7925aa0e766c8
Hyojun Cho <lionjerher84@gmail.com>
2023-12-15 01:25:30 +0900

    네트워트 통신 - 연습문제 5 - 병렬처리 제출

--- lib/12_14/ex_download_image.dart
+++ lib/12_14/ex_download_image.dart
@@ -4,7 +4,13 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
 void main() {
-  downloadAndSaveImage();
+
+  /// 이미지 다운로드 예제
+  // downloadAndSaveImage();
+
+
+  /// 병렬처리 속도 테스트 예제
+  compareDownloadTimes();
 }
 
 void downloadAndSaveImage() async {
@@ -89,4 +95,74 @@ Future<void> saveImageToFile(Uint8List imageBytes, String savePath) async {
 String formatDuration(Duration duration) {
   final formatter = DateFormat('H:mm:ss.S');
   return formatter.format(DateTime(0).add(duration));
+}
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+/// 병렬처리 속도 테스트
+void compareDownloadTimes() async {
+  DateTime startTime, endTime;
+  Duration sequentialDuration, parallelDuration;
+
+  print(' 나사 , 5K (3788 × 5077) 3.6M 이미지 20번씩 다운로드 시간 체크하기 --- ');
+
+  // 일반 다운로드 처리 방식
+  startTime = DateTime.now();
+  await awaitTest();
+  endTime = DateTime.now();
+  sequentialDuration = endTime.difference(startTime);
+
+  // 병렬 처리 방식
+  startTime = DateTime.now();
+  await parallelTest();
+  endTime = DateTime.now();
+  parallelDuration = endTime.difference(startTime);
+
+  print('일반 다운로드 처리 방식: ${formatDuration(sequentialDuration)}');
+  print('병렬 처리 방식: ${formatDuration(parallelDuration)}');
+
+  if (sequentialDuration < parallelDuration) {
+    print('일반 다운로드 처리 방식이 병렬 처리 방식보다 빠릅니다.');
+  } else if (sequentialDuration > parallelDuration) {
+    print('병렬 처리 방식이 일반 다운로드 처리 방식보다 빠릅니다.');
+  } else {
+    print('일반 다운로드 처리 방식과 병렬 처리 방식의 소요 시간이 동일합니다.');
+  }
+}
+
+/// 일반 다운로드 처리 - 다운로드
+Future<void> awaitTest() async {
+  await download();  await download();  await download();  await download();  await download();
+  await download();  await download();  await download();  await download();  await download();
+  await download();  await download();  await download();  await download();  await download();
+  await download();  await download();  await download();  await download();  await download();
+}
+
+/// 병렬 처리 - 다운로드
+Future<void> parallelTest() async {
+  List<Future> futures = [
+    download(),    download(),    download(),    download(),    download(),
+    download(),    download(),    download(),    download(),    download(),
+    download(),    download(),    download(),    download(),    download(),
+    download(),    download(),    download(),    download(),    download(),
+  ];
+  await Future.wait(futures);
+}
+
+/// 다운로드 메소드
+Future<void> download() async {
+  // 나사 , 5K (3788 × 5077) 3.6M
+  String url = 'https://live.staticflickr.com/65535/53395722120_40af9685c5_5k.jpg'; // 다운로드할 이미지 URL
+  try {
+    Uint8List imageBytes = await downloadImage(url);
+    if (imageBytes != null) {
+      // 이미지 다운로드 성공한 경우 처리
+    } else {
+      // 이미지 다운로드 실패한 경우 처리
+      throw Exception('이미지 다운로드 실패');
+    }
+  } catch (e) {
+    // 다운로드 및 처리 중 오류 발생한 경우 처리
+    print('이미지 다운로드 및 처리 중 오류가 발생했습니다: $e');
+  }
 }
